81230528e982dd33ce67edc174d49a0fb37520d3
Dipto Biswas <dbiswas@uoguelph.ca>
2024-08-06 10:35:13 -0400

    fixed: production build not retreiving

--- app/api/case-studies/route.ts
+++ app/api/case-studies/route.ts
@@ -3,7 +3,8 @@ import { NextResponse } from 'next/server';
 export const runtime = 'edge';
 
 export async function GET() {
-  const res = await fetch('http://localhost:3000/case_studies.json');
+  const apiUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : process.env.NEXT_PUBLIC_API_URL;
+  const res = await fetch(`${apiUrl}/case_studies.json`);
   const data = await res.json();
   return NextResponse.json(data);
 }
